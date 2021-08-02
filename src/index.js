a2805b7043dfe871b8ccb75d689489334f2c508c
John Paulo Panganiban <olopangs@gmail.com>
2021-08-02 13:45:03 +0800

    ADD: Redux boilerplate setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux' // para may magrab tayo sa store
+import store from './redux/store';
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}> 
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/userReducer.js
@@ -0,0 +1,29 @@
+// reducer is a function that gets two property
+// state object, last state or initial state that we're trying to store
+// then receives an action
+
+const initialState = {
+    currentUser: null
+}
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                // new state 
+                ...state, // everything else on the state so we use the ...spread
+                currentUser: action.payload,
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
+
+// ACTIONS
+
+export const setCurrentUser = user => ({
+    type: 'SET_CURRENT_USER',
+    payload: user
+})
